268bd733d8b0909e09fe575a27519fca848609a2
saaperezru <saaperezru@gmail.com>
2013-10-19 18:03:01 -0400

    Added the concept of a Schedule

--- horarios/Models.py
+++ horarios/Models.py
@@ -8,7 +8,7 @@ class Subject():
         self.groups=grouops
 
 class Group():
-    def __init__(self,teacher,schedule):
+    def __init__(self,code,teacher,schedule):
         """ 
             rooms is a dictionary of day : room
             schedule is a dictionary of day : begin-end
@@ -30,3 +30,34 @@ class Group():
             hours[i-1]=1
         return int(hours,2)
         
+class Schedule():
+    def __init__(self,busy,*args):
+        """
+        busy is an array of integers, each integer is one day's hours
+        """
+        self.groups = []
+        if(busy != None):
+            self.busy = busy
+        else:
+            self.busy=[0]*7
+
+        for arg in args:
+            if(isinstance(arg,list)):
+                for i in arg:
+                    self.addGroup(i)
+            else:
+                self.addGroup(arg)
+
+    def clone(self):
+        pass
+
+
+    def isCompatible(self,course):
+        for i in zip(self.busy,course.schedule):
+            if(i[0]&i[1] != 0) return False
+        return True
+
+    def addGroup(self,group):
+        self.group.append(group)
+        for i in range(0,len(self.busy)):
+            self.busy[i] = self.busy[i] | group.schedule[i]
