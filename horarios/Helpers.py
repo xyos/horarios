d6b9ea480ec936cb5a600a39fa12521967d5bea1
saaperezru <saaperezru@gmail.com>
2013-10-19 15:25:40 -0400

    Interface against SIA's JSON API. For now the SIA object allows you to retrieve courses, groups and verify existance by name

---
+++ horarios/Helpers.py
@@ -0,0 +1,35 @@
+import json
+import urllib2
+
+class SIA:
+
+    def existsCourse(this,name,level):
+        return this.queryNumCoursesWithName(name,level)>0
+
+    def queryNumCoursesWithName(this,name,level):
+        data = json.dumps({"method": "buscador.obtenerAsignaturas", "params": [name, level, "", level, "", "", 1, 1]})
+        req = urllib2.Request("http://www.sia.unal.edu.co/buscador/JSON-RPC", data, {'Content-Type': 'application/json'})
+        f = urllib2.urlopen(req)
+        result = json.loads(f.read())["result"]["totalAsignaturas"]
+        f.close()
+        return result
+
+    def queryCourseByName(this,name,level,maxRetrieve):
+        data = json.dumps({"method": "buscador.obtenerAsignaturas", "params": [name, level, "", level, "", "", 1, maxRetrieve]})
+        req = urllib2.Request("http://www.sia.unal.edu.co/buscador/JSON-RPC", data, {'Content-Type': 'application/json'})
+        f = urllib2.urlopen(req)
+        result=json.loads(f.read())
+        f.close()
+        return result["result"]
+
+    def queryGroupsByCourseCode(this,code):
+        data = json.dumps({"method": "buscador.obtenerGruposAsignaturas", "params": [code, "0"]})
+        req = urllib2.Request("http://www.sia.unal.edu.co/buscador/JSON-RPC", data, {'Content-Type': 'application/json'})
+        f = urllib2.urlopen(req)
+        result=json.loads(f.read())
+        f.close()
+        return result["result"]
+
+             
+sia = SIA()
+result = sia.queryGroupsByCourseCode("2016696")
