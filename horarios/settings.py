6de25e3a0ca5f9c13a86cd42916baae215fd8f5f
Jairo Suarez <jasuarezpr@gmail.com>
2013-10-26 15:52:13 -0500

    settings to host static files

--- horarios/settings.py
+++ horarios/settings.py
@@ -64,17 +64,19 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = PROJECT_PATH +  '/static/'
+# STATIC_ROOT = PROJECT_PATH +  '/static/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
 STATIC_URL = '/static/'
 
 # Additional locations of static files
+STATIC_PATH = PROJECT_PATH +  '/static/'
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    STATIC_PATH,
 )
 
 # List of finder classes that know how to find static files in
