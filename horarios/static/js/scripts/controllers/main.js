76a4a77c2772abdc1d5f4bbbe795627cf73106af
Jairo Suárez <jasuarezpr@gmail.com>
2013-10-30 19:05:03 -0700

    Adding a list to contain selected subjects. Selected subjects by the auto-complete are added to this list and are in the subjects view scope.

--- horarios/static/js/scripts/controllers/main.js
+++ horarios/static/js/scripts/controllers/main.js
@@ -2,12 +2,17 @@
 
 angular.module('schedulesApp')
 .controller('TypeaheadCtrl', function ($scope, $http, limitToFilter) {
+  $scope.selectedSubjects = [];
   $scope.subjects = function(subjectName){
     return $http.get('/api/v1.0/subject/autocomplete/' + subjectName + "/?format=json")
     .then(function(response){
       console.log(limitToFilter(response.data,15));
       return limitToFilter(response.data,15);
     });
+  };
+  $scope.onSelect = function ($item, $model, $label) {
+    console.log($item);
+    $scope.selectedSubjects.push($item);
   }
 });
 
