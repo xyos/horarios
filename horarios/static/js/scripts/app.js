e43810d7e66560ced696ebf26c6c126a58f91530
Jairo Suárez <jasuarezpr@gmail.com>
2013-11-02 10:25:52 -0700

    showing busy hours in schedule.detail

--- horarios/static/js/scripts/app.js
+++ horarios/static/js/scripts/app.js
@@ -12,3 +12,16 @@ angular.module('schedulesApp', [
     $rootScope.$stateParams = $stateParams;
   }
 ]);
+
+angular.module('schedulesApp')
+.service('sharedSchedule',function(){
+  var scheduleValue = {"busy": [0, 3264, 192, 3264, 192, 0, 0], "groups": [["1", [0, 0, 192, 0, 192, 0, 0]], ["1", [0, 3072, 0, 3072, 0, 0, 0]], ["1", [0, 192, 0, 192, 0, 0, 0]]]};
+  return {
+    getSchedule : function() {
+      return scheduleValue;
+    },
+    setSchedule : function(schedule){
+      scheduleValue = schedule;
+    }
+  }
+});
--- horarios/static/js/scripts/controllers/main.js
+++ horarios/static/js/scripts/controllers/main.js
@@ -13,15 +13,47 @@ angular.module('schedulesApp')
     $scope.selectedSubjects.push($item);
   }
 })
-.controller('ScheduleDetailCtrl',function($scope, $http){
+.controller('ScheduleDetailCtrl',function($scope, $http, sharedSchedule){
+  console.log(sharedSchedule.getSchedule());
+  $scope.scheduleItems = sharedSchedule.getSchedule();
   $scope.daysOfWeek =
     ['lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado', 'domingo'];
   $scope.hours = [];
   for (var i = 7; i <= 20; i++) {
     $scope.hours.push(i + ":00");
   };
+  $scope.parseSchedule = function(daysOfWeek, hours, scheduleItems){
+    var schedule = [];
+    var dayHeaders = [{text : 'hora', class : 'heading'}];
+    daysOfWeek.forEach(function(item){
+      dayHeaders.push({text : item, class : 'heading'});
+    });
+    schedule.push(dayHeaders);
+    var busyArray = [];
+    scheduleItems.busy.forEach(function(item){
+      //adding a 1 to the decimal to keep the preeceding zeros
+      busyArray.push(( item + 4096 ).toString(2).substring(2));
+    });
+    console.log(busyArray);
+    for (var i = 0; i < hours.length; i++) {
+      var row = [];
+      row.push({text : hours[i], class : 'heading'});
+      for (var j = 0; j < daysOfWeek.length; j++) {
+        //adding busy hours
+        row.push({text: ( (busyArray[j][i] == "1") ? "ocupado" : "" ),
+                 class: "",
+                 busy : ( (busyArray[j][i] == "1") ? 1 : 0 )
+        });
+      };
+      schedule[i+1] = row;
+    };
+    return schedule;
+  }
+  $scope.schedule =
+    $scope.parseSchedule($scope.daysOfWeek, $scope.hours, $scope.scheduleItems);
+  console.log($scope.schedule);
 })
-.controller('ScheduleListCtrl',function($scope, $http){
+.controller('ScheduleListCtrl',function($scope, $http, sharedSchedule){
   $scope.getSchedules = function(){
     return $http.get('/api/v1.0/schedule/random/?format=json')
     .then(function(response){
@@ -29,4 +61,5 @@ angular.module('schedulesApp')
     });
   };
  $scope.schedules = $scope.getSchedules();
+ //sharedSchedule.setSchedule($scope.schedules[0]);
 });
