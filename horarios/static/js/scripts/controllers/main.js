ebee2b372140a2df2c535bffbbd7c93d5b000bad
Jairo Suárez <jasuarezpr@gmail.com>
2013-11-02 01:30:23 -0700

    changing to bootstrap 3 conventions

--- horarios/static/js/scripts/controllers/main.js
+++ horarios/static/js/scripts/controllers/main.js
@@ -4,9 +4,8 @@ angular.module('schedulesApp')
 .controller('SubjectsCtrl', function($scope, $http, limitToFilter) {
   $scope.selectedSubjects = [];
   $scope.subjects = function(subjectName){
-    return $http.get('/api/v1.0/subject/autocomplete/' + subjectName + "/?format=json")
+    return $http.get('/api/v1.0/subject/autocomplete/' + subjectName + '/?format=json')
     .then(function(response){
-      console.log(limitToFilter(response.data,15));
       return limitToFilter(response.data,15);
     });
   };
@@ -18,7 +17,16 @@ angular.module('schedulesApp')
   $scope.daysOfWeek =
     ['lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado', 'domingo'];
   $scope.hours = [];
-  for (var i = 6; i < 22; i++) {
+  for (var i = 7; i <= 20; i++) {
     $scope.hours.push(i + ":00");
   };
+})
+.controller('ScheduleListCtrl',function($scope, $http){
+  $scope.getSchedules = function(){
+    return $http.get('/api/v1.0/schedule/random/?format=json')
+    .then(function(response){
+      return response.data;
+    });
+  };
+ $scope.schedules = $scope.getSchedules();
 });
