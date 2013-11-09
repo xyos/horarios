7fc5c7841ba7f45c0ffab44a000057d87227e3c2
Santiago Perez-Rubiano <saaperezru@gmail.com>
2013-11-09 05:25:57 -0500

    Postgres is more strict

--- horarios/models.py
+++ horarios/models.py
@@ -9,7 +9,7 @@ class Teacher(models.Model):
     name = models.CharField(max_length=100)
 
 class Group(models.Model):
-    schedule = models.CommaSeparatedIntegerField(max_length=10)
+    schedule = models.CommaSeparatedIntegerField(max_length=100)
     code = models.IntegerField()
     teacher = models.ForeignKey(Teacher)
     subject = models.ForeignKey(Subject)
