9ec6e61fbdac299ed676b27cd6279a1928652da3
Jairo Suarez <jasuarezpr@gmail.com>
2015-07-19 12:20:18 -0500

    Adding a command to clear subjects

---
+++ horarios/management/commands/clear_subjects.py
@@ -0,0 +1,12 @@
+from django.core.management.base import BaseCommand
+from horarios.models import Subject, Group, Teacher, Profession
+class Command(BaseCommand):
+
+    help = 'resets subjects, groups and professions'
+
+    def handle(self, *args, **options):
+        Group.objects.all().delete()
+        Teacher.objects.all().delete()
+        Profession.objects.all().delete()
+        Subjects.objects.all().delete()
+        print("cleared!")
